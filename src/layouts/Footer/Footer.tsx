700ee42ed2e85bffa5d0f92ff0bbe0669529ebb8
gergana.ivvanova <gergana.ivvanova@gmail.com>
2022-04-19 18:06:12 +0300

    fix: footer overflow

--- src/layouts/Footer/Footer.tsx
+++ src/layouts/Footer/Footer.tsx
@@ -1,24 +1,17 @@
-import Box from '@mui/material/Box'
-import ListItem from '@mui/material/ListItem'
 import footerLinks from './footerLinks'
 import './Footer.scss'
 
 function Footer(): JSX.Element {
     return (
-        <Box component="footer" className="footer">
-            <Box component="div" className="footer__container">
-                <Box component="ul" className="footer__container-links">
-                    {footerLinks.map((link) => (
-                        <ListItem
-                            key={link.name}
-                            className="footer__container-link"
-                        >
-                            <a href={link.path}>{link.name}</a>
-                        </ListItem>
-                    ))}
-                </Box>
-            </Box>
-        </Box>
+        <footer className="footer">
+            <ul className="footer__container">
+                {footerLinks.map((link) => (
+                    <li key={link.name} className="footer__container-link">
+                        <a href={link.path}>{link.name}</a>
+                    </li>
+                ))}
+            </ul>
+        </footer>
     )
 }
 
